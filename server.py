1df1839380e00ff286416d6f1a60353f4eb60231
Brad Miller <bonelake@mac.com>
2017-06-18 11:27:55 -0500

    Add course model in SQLAlchemy for testing

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, redirect
 from course import get_base, get_version
 import logging
 import os.path
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://millbr02:@localhost/runestone'
+db = SQLAlchemy(app)
 
-rslogger = logging.getLogger('runestone.dev')
-rslogger.setLevel('DEBUG')
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String, unique=True)
+    term_start_date = db.Column(db.Date)
+    base_course = db.Column(db.String)
+    python3 = db.Column(db.Boolean)
+    login_required = db.Column(db.Boolean)
+
+db.create_all()
+
 
 @app.route('/')
 def hello_world():
@@ -35,16 +47,19 @@ def serve_page(course, pageinfo):
     :param pageinfo: the path to the page that is desired to be served
     :return: filled page template
     '''
+    the_course = Course.query.filter_by(course_name=course).first()
+    if not the_course:
+        return redirect(f'http://runestone/errors/nocourse/{course}')
+
     app.logger.debug(pageinfo)
-    app.logger.debug(request.url)
-    base_course = get_base(course)
+    app.logger.debug(type(the_course.python3))
+    base_course = the_course.base_course
     course_version = get_version(course)
-    python3 = True
-    login_required = True
+    python3 = 'true' if the_course.python3 == 'T' else 'false'
+    login_required = 'true' if the_course.login_required == 'T' else 'false'
 
     template = os.path.join(base_course, pageinfo)
-    return render_template(template, context=dict(basecourse=base_course, python3=python3, loginrequired=login_required))
-
+    return render_template(template, basecourse=base_course, python3=python3, login_required=login_required)
 
 
 
