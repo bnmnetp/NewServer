8e1c4fb1a5ec3ceb0a57e3abf1f8f3e8d04e4be5
Bradley Miller <bmiller@luther.edu>
2017-06-20 07:01:32 -0500

    start on logging API

--- runestone/api/endpoints.py
+++ runestone/api/endpoints.py
@@ -1,5 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response
+from ..model import LogInfo
+import uuid
+import datetime
+import json
+
 
 api = Blueprint('api', __name__, url_prefix='/api')
+from runestone import db,app
 
+@api.route('/hsblog')
+def log_book_event():
+    setCookie = False
+    sid = request.cookies.get('ipuser')  # quickest way to get the sid
+
+    # if auth.user:    # todo: How can we make web2py and flask share login information? - we can get the session cookie but we need to unscramblepython
+    #     sid = auth.user.username
+    #     compareAndUpdateCookieData(sid)
+    #     setCookie = True    # we set our own cookie anyway to eliminate many of the extraneous anonymous
+    #                         # log entries that come from auth timing out even but the user hasn't reloaded
+    #                         # the page.
+    # else:
+    #     if request.cookies.has_key('ipuser'):
+    #         sid = request.cookies['ipuser'].value
+    #         setCookie = True
+    #     else:
+    #         sid = str(uuid.uuid1().int)+"@"+request.client
+    #         setCookie = True
+    act = request.args.act
+    div_id = request.args.div_id
+    event = request.args.event
+    course = request.args.course
+    ts = datetime.datetime.now()
+    tt = request.args.time
+    if not tt:
+        tt = 0
+
+    try:
+        li = LogInfo(sid=sid, act=act, div_id=div_id, event=event, timestamp=ts, course_id=course)
+        db.add(li)
+        db.commit()
+    except:
+        app.logger.debug('failed to insert log record for {} in {} : {} {} {}'.format(sid, course, div_id, event, act))
+
+    response = make_response()
+    response.headers['content-type'] = 'application/json'
+    res = {'log':True}
+    if setCookie:
+        response.cookies['ipuser'] = sid
+        response.cookies['ipuser']['expires'] = 24*3600*90
+        response.cookies['ipuser']['path'] = '/'
+    return json.dumps(res)
 
--- runestone/model.py
+++ runestone/model.py
@@ -10,4 +10,14 @@ class Course(db.Model):
     python3 = db.Column(db.Boolean)
     login_required = db.Column(db.Boolean)
 
+# Regex to convert web2py to SQLAlchemy - Field\('(\w+)',\s*'(\w+)'\), --> $1 = db.Column(db.$2)
+class LogInfo(db.Model):
+    __tablename__ = 'useinfo'
+    timestamp = db.Column(db.Datetime)
+    sid = db.Column(db.String)
+    event = db.Column(db.String)
+    act = db.Column(db.String)
+    div_id = db.Column(db.String)
+    course_id = db.Column(db.String)
+
 db.create_all()
